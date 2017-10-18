05b4b21c3e24193db1ef1acc98690805c4b860cc
Volodymyr Sharayenko <v.sharayenko@samsung.com>
2017-10-18 18:49:27 +0300

    updated comments

--- JBE/JBE_7/src/com/alex/jbe/ex7/Main.java
+++ JBE/JBE_7/src/com/alex/jbe/ex7/Main.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 
 public class Main {
 
-	 static int getData(String text, Scanner inStream) {
-	        System.out.print(text);
-	        return inStream.nextInt();
-	 }
+     static int getData(String text, Scanner inStream) {
+            System.out.print(text);
+            return inStream.nextInt();
+     }
 
-	 public static void main(String[] args) {
-    	  Scanner inStream = new Scanner(System.in);
+     public static void main(String[] args) {
+          Scanner inStream = new Scanner(System.in);
 
           int value = getData("Input number: ", inStream);
 
           for (int i = 1; i <= 10; i++) {
-        	  /* изучи как формируется строка которая печатается:
-        	   * она состоит из подстрок которые соеденяются (конкатенация) оператором `+`
-        	   * 	String a = "1";
-        	   * 	String b = "2";
-        	   * 	String c = a + b; //c == "12";
-        	   * Java умеет на лету преобразовывать некоторые типы к String
-        	   * в выражение ниде Java автоматом преобразует все int в String и соеденяет все String благодаря оператору "+"
-        	   */
-        	  System.out.println(value +" x " + i + " = " + value * i);
+              /* изучи как формируется строка которая печатается:
+               * она состоит из подстрок которые соеденяются (конкатенация) оператором `+`
+               *    String a = "1";
+               *    String b = "2";
+               *    String c = a + b; //c == "12";
+               * Java умеет на лету преобразовывать некоторые типы к String
+               * в выражение ниде Java автоматом преобразует все int в String и соеденяет все String благодаря оператору "+"
+               */
+              System.out.println(value +" x " + i + " = " + value * i);
           }
-	 }
+     }
 }
