c01f2ec06b04e98e8498c407754968c1a041f10a
Volodymyr Sharayenko <v.sharayenko@samsung.com>
2017-10-18 16:55:31 +0300

    Added link to the original tasks for first iteration

--- JBE/JBE_7/src/com/alex/jbe/ex7/Main.java
+++ JBE/JBE_7/src/com/alex/jbe/ex7/Main.java
@@ -3,23 +3,20 @@ package com.alex.jbe.ex7;
 import java.util.Scanner;
 
 public class Main {
-	
-	
+
 	 static int getData(String text, Scanner inStream) {
 	        System.out.print(text);
 	        return inStream.nextInt();
 	 }
-        public static void main(String[] args) {
-        	  Scanner inStream = new Scanner(System.in);
-              
-              String text = "Input number: ";
-              int value1 = getData(text, inStream);
-              
-              for (int i=1; i< 11; i++){
-              
-              System.out.println(value1 *i);
-              
-              
+
+	 public static void main(String[] args) {
+    	  Scanner inStream = new Scanner(System.in);
+
+          String text = "Input number: ";
+          int value1 = getData(text, inStream);
+
+          for (int i=1; i< 11; i++) {
+        	  System.out.println(value1 *i);
+          }
+	 }
 }
-}
-}	
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # java-edu
 Repository for java education projects
+
+## Iteration 1: Java Basic Exercises
+https://www.w3resource.com/java-exercises/basic/index.php
