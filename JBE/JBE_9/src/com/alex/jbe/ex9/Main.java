39283507ad0f2fd53f8936d54c96d59c8c3cb561
Volodymyr Sharayenko <v.sharayenko@samsung.com>
2017-10-20 12:50:58 +0300

    Added Oleksiy versions of JBE8 + 9

---
+++ JBE/JBE_9/src/com/alex/jbe/ex9/Main.java
@@ -0,0 +1,20 @@
+package com.alex.jbe.ex9;
+
+public class Main
+{
+
+	public static void main(String args[])	
+	{
+		
+	   final double a = 25.5;  
+	   final double b = 3.5;
+	   final double c = 40.5;
+	   final double d = 4.5;
+	   double z = ((a*b - b*b)/( c-d));
+	   
+	   
+	   System.out.print(z);
+	  	
+		
+	}
+}
