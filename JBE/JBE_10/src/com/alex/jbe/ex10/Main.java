0d01ff456a8cb3ca34f3f083576f528d457c23cc
Kiker85 <32679373+Kiker85@users.noreply.github.com>
2017-10-20 22:03:45 +0300

    Add files via upload

---
+++ JBE/JBE_10/src/com/alex/jbe/ex10/Main.java
@@ -0,0 +1,17 @@
+package com.alex.jbe.ex10;
+
+public class Main
+{
+
+	public static void main(String args[])	
+	{
+		double x = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+        System.out.println(x);
+		
+		
+		
+		
+		
+		
+	}
+}
