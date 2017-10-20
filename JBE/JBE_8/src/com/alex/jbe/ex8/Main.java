f8add7379f832853e6f7b183e064b2ed4404e39f
Volodymyr Sharayenko <v.sharayenko@samsung.com>
2017-10-20 13:21:30 +0300

    added more links & comments

--- JBE/JBE_8/src/com/alex/jbe/ex8/Main.java
+++ JBE/JBE_8/src/com/alex/jbe/ex8/Main.java
@@ -1,18 +1,50 @@
 package com.alex.jbe.ex8;
 
+import java.util.ArrayList;
+
 public class Main
 {
 
-	public static void main(String args[])	
+	private static void print1()
 	{
-		final String text1 = "     J    a   v     v   a";
-		final String text2 = "    J   a a   v   v   a a";
-		final String text3 = "J   J  aaaaa   V V   aaaaa";
-		final String text4 = " JJ   a     a   V   a     a";
-		System.out.println(text1 + " \n " + text2 + " \n " + text3 + " \n " + text4 );
-		
-		
-		
-		
-}
+		final String text1 = "   J    a   v     v   a";
+		final String text2 = "   J   a a   v   v   a a";
+		final String text3 = "J  J  aaaaa   V V   aaaaa";
+		final String text4 = " JJ  a     a   V   a     a";
+		System.out.println(text1 + "\n" + text2 + "\n" + text3 + "\n" + text4 );
+	}
+
+	private static void print2()
+	{
+		System.out.println("   J    a   v     v   a");
+		System.out.println("   J   a a   v   v   a a");
+		System.out.println("J  J  aaaaa   V V   aaaaa");
+		System.out.println(" JJ  a     a   V   a     a");
+	}
+
+	private static void print3()
+	{
+		//вариант реализации Списка Строк - https://habrahabr.ru/post/128269/
+		ArrayList<String> str = new ArrayList<String>();
+		str.add("   J    a   v     v   a");
+		str.add("   J   a a   v   v   a a");
+		str.add("J  J  aaaaa   V V   aaaaa");
+		str.add(" JJ  a     a   V   a     a");
+
+		//цикл for each - поддерживает обход по Java Collections - https://vertex-academy.com/tutorials/ru/cikl-for-each/
+		for(String s : str) {
+			System.out.println(s);
+		}
+	}
+
+	public static void main(String args[])
+	{
+		System.out.print("Variant 1:\n");
+		print1();
+		System.out.print("Variant 2:\n");
+		print2();
+		System.out.print("Variant 2:\n");
+		print3();
+	}
+
 }
--- JBE/JBE_9/src/com/alex/jbe/ex9/Main.java
+++ JBE/JBE_9/src/com/alex/jbe/ex9/Main.java
@@ -3,18 +3,14 @@ package com.alex.jbe.ex9;
 public class Main
 {
 
-	public static void main(String args[])	
+	public static void main(String args[])
 	{
-		
-	   final double a = 25.5;  
+
+	   final double a = 25.5;
 	   final double b = 3.5;
 	   final double c = 40.5;
 	   final double d = 4.5;
-	   double z = ((a*b - b*b)/( c-d));
-	   
-	   
+	   double z = (a * b - b * b) / (c - d);
 	   System.out.print(z);
-	  	
-		
 	}
 }
